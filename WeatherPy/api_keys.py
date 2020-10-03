a43b10387446f144269b113420a9f2fa386ab2ea
anand-sharan <a.v.sharan@gmail.com>
2020-10-03 15:15:39 -0700

    readme

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "397eb3818b564ad73bb0e0d5f86becde"
+weather_api_key = "YOUR KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyBXCWBNf1KdsHEALAmm1iPA2uGTj0jNwA0"
+g_key = "YOUR KEY HERE"
